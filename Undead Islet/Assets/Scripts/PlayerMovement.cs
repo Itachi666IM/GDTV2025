eab8785e185f3862f863b955b38119cf39cad005
Sanj1t <79507387+Itachi666IM@users.noreply.github.com>
2025-05-23 00:26:30 +0530

    Added Sword Effects

--- Undead Islet/Assets/Scripts/PlayerMovement.cs
+++ Undead Islet/Assets/Scripts/PlayerMovement.cs
@@ -19,10 +19,35 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 turn;
     public GameObject sword;
+    Animator anim;
+    public ParticleSystem slashEffect;
+
+    public AudioSource sfx;
+    public AudioClip slashSound;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Rotate();
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("attack");
+        }
+        
+    }
 
+    public void PlaySlashEffect()
+    {
+        slashEffect.Play();
+        sfx.PlayOneShot(slashSound);
+    }
+
+    void Movement()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -43,8 +68,11 @@ public class PlayerMovement : MonoBehaviour
         }
 
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
         turn.x += Input.GetAxis("Mouse X");
         turn.y += Input.GetAxis("Mouse Y");
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
